c7a2659d95459ecdff3ecd7afdc05386101e6e5a
Lachlan Campbell <lachlan@hackclub.com>
2020-05-12 18:49:44 -0400

    Fix icon

--- components/icon.js
+++ components/icon.js
@@ -1,6 +1,5 @@
-import { Box } from 'theme-ui'
+/** @jsx jsx */
+import { jsx } from 'theme-ui'
 import Icon from '@hackclub/icons'
 
-export default ({ sx, ...props }) => (
-  <Box as={Icon} {...props} sx={{ lineHeight: 0, ...sx }} />
-)
+export default (props) => <Icon {...props} />
