561b305ec2a59e63e23c60606459301a7b676c2e
Lachlan Campbell <lachlan@hackclub.com>
2020-05-13 04:21:01 +0000

    Merge commits

--- pages/_app.js
+++ pages/_app.js
@@ -3,7 +3,6 @@ import NextApp from 'next/app'
 
 import '../public/fonts.css'
 import { ThemeProvider } from 'theme-ui'
-import { Global } from '@emotion/core'
 import theme from '../lib/theme'
 import Meta from '../components/meta'
 import Layout from '../components/layout'
@@ -14,19 +13,6 @@ export default class App extends NextApp {
     return (
       <ThemeProvider theme={theme}>
         <Meta />
-        <Global
-          styles={(theme) => ({
-            body: {
-              fontFamily: theme.fonts.body,
-              lineHeight: theme.lineHeights.body,
-              fontWeight: theme.fontWeights.body,
-              color: theme.colors.text,
-              backgroundColor: theme.colors.background,
-              margin: 0,
-              minHeight: '100vh',
-            },
-          })}
-        />
         <Layout>
           <Component {...pageProps} />
         </Layout>
